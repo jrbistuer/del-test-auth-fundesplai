4f30e4cf70577ed39a867849a6b623ea7164b4eb
jrbistuer <jrbistuer@gmail.com>
2025-06-04 08:54:47 +0200

    no data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,19 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "del-test-react", appId: "1:681008159334:web:7b4cb542ac18cfe17feba7", storageBucket: "del-test-react.firebasestorage.app", apiKey: "AIzaSyCHAdFc5DZtuMfXNp8oDuooG-4zFS6YbV4", authDomain: "del-test-react.firebaseapp.com", messagingSenderId: "681008159334", measurementId: "G-X1PSNPRKL4" })), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "",
+      appId: "",
+      storageBucket: "",
+      apiKey: "",
+      authDomain: "",
+      messagingSenderId: "",
+      measurementId: ""})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
+  ]
 };
