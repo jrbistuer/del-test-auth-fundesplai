e56a871303dd1afb92c5a08be4912248e42731ce
jrbistuer <jrbistuer@gmail.com>
2025-06-04 08:47:48 +0200

    'def'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "del-test-react", appId: "1:681008159334:web:7b4cb542ac18cfe17feba7", storageBucket: "del-test-react.firebasestorage.app", apiKey: "AIzaSyCHAdFc5DZtuMfXNp8oDuooG-4zFS6YbV4", authDomain: "del-test-react.firebaseapp.com", messagingSenderId: "681008159334", measurementId: "G-X1PSNPRKL4" })), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: LoginComponent,
+		...canActivate(redirectLoggedInToHome)
+	},
+	{
+		path: 'home',
+		component: HomeComponent,
+		...canActivate(redirectUnauthorizedToLogin)
+	},
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
+];
